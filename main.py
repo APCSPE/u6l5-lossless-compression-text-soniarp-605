74e10ff715cf2412849e7616c0613033bee69f2b
soniarp-605 <148896719+soniarp-605@users.noreply.github.com>
2024-03-25 13:15:51 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,32 @@
-#WRITE YOUR PROGRAM HERE 
-print('hello world')
+def RLE_compress(message):
+  mlist = list(message)
+  result = " "
+  count = 0
+  for i in range [len(mlist)-1]:
+    letter1 = mlist[i]
+    letter2 = mlist[i+1]
+    if letter2 == letter1:
+      count += 1
+    else:
+      result = result + str(letter1) + str(count)
+      count = 1
+      
+  letterlast = mlist[len(mlist)-1]
+  result = result + letterlast + str(count)
+  resturn(result)
+
+
+
+# TEST CODE:
+print(RLE_compress("AABBBAAAABBBBBAAAAAABBBBBBB"))
+print(RLE_compress("AACCCCBBBBBAAAAAAAXFFFFFFFF"))
+print(RLE_compress("AACCCCBBBBBAAAAAAAXFFFFFFFFD"))
+print(RLE_compress("AACCCCBBBBBAAAAAAAXFFFFFFFFDD"))
+print(RLE_compress("AACCCCBBBBBAAAAAAAXFFFFFFFFDDD"))
+print(RLE_compress("ABCDEF"))
+print(RLE_compress("FFFFFFFFFFFFFFFFFFF"))
+print(RLE_compress("F"))
+print(RLE_compress("F????"))
+print(RLE_compress("Mmmmmmmmmm sooooo goooooood!"))
+print(RLE_compress("Booooooooooooo, hisssssssss"))
+
